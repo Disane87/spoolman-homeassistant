8ce5abd6a90b77ab4f233305ae5475623c042aa0
Marco Franke <disane@icloud.com>
2023-11-01 19:48:45 +0000

    fix: :bug: fixed a bug where location from spoolman is empty
    fixes #44

--- custom_components/spoolman/sensor.py
+++ custom_components/spoolman/sensor.py
@@ -72,8 +72,9 @@ class Spool(CoordinatorEntity, SensorEntity):
         self._attr_icon = ICON
 
         location_name = (
-            self._spool["location"] if spool_data["archived"] is False else "Archived"
-        )
+            "Unknown" if not self._spool.get("location") else self._spool["location"]
+        ) if spool_data["archived"] is False else "Archived"
+
 
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, conf_url, location_name)},  # type: ignore
