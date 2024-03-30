eda5f329dd7708c2b2e997923acc7f966cd6504e
Marco Franke <mfranke87@icloud.com>
2024-03-30 09:35:03 +0000

    fix: Fixes an issue when there is no active spool in klipper
    fixes Cant configure integration without active spool #105

--- custom_components/spoolman/classes/klipper_api.py
+++ custom_components/spoolman/classes/klipper_api.py
@@ -13,14 +13,19 @@ class KlipperAPI:
         """Initialize the Klipper API."""
         self.base_url = add_trailing_slash(base_url)
 
-    async def get_active_spool_id(self) -> int:
+    async def get_active_spool_id(self) -> int | None:
         """Get active spool from Klipper API."""
         async with aiohttp.ClientSession() as session:
             url = f"{self.base_url}server/spoolman/spool_id"
             async with session.get(url) as response:
                 if response.status == 200:
                     data = await response.json()
-                    return int(data.get('result').get("spool_id"))
+                    # Extract the spool_id from the data dictionary
+                    spool_id = data.get('result', {}).get("spool_id")
+
+                    # Convert spool_id to an integer if it is not None; otherwise, return None
+                    return int(spool_id) if spool_id is not None else None
+
                 else:
                     return 0
     async def api_version(self) -> str | None:
